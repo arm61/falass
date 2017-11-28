a70fbdb5d7436150f6a448fa8d777b8d489d9eb0
arm61 <arm61@bath.ac.uk>
2017-11-28 16:14:09 +0000

    Getting setup.py set up

---
+++ README.txt
@@ -0,0 +1,5 @@
+falass is a pure python program designed to obtain neutron or x-ray reflectometry data from a molecular simulation output.
+
+This code will read in a PDB structure, allow the input of scattering lengths for each of the atom types, build the system into a series of layers and calculate the reflectometry based on the Abeles optical matrix formalism.
+
+Further it is then possible to compare the calculated reflectometry with that from a experimental data file.
--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ MICRO = 1
 ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
+from distutils.core import setup
+
 # falass setup
-info = {
-        'name': 'falass', 
-        'description': 'Neutron and X-ray Reflectometry from Simulation',
-        'author': 'Andrew R. McCluskey',
-        'author_email': 'arm61@bath.ac.uk',
-        'platforms': ['Linux'], 
-        }
+setup(
+    name='falass',
+    version=VERSION,
+    description='Neutron and X-ray Reflectometry from Computer Simulation',
+    author='Andrew R. McCluskey',
+    author_email='arm61@bath.ac.uk',
+    long_description=open('README.txt').read(),
+    license='MIT',
+)
